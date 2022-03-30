a8b3d673d17e87833fa2cbcbfc85564cd79cb6d3
pon <49616225+AJpon@users.noreply.github.com>
2022-03-30 21:46:47 +0900

    Auto Instant Replay config edit

--- BetterGFE/SettingWindow.xaml.cs
+++ BetterGFE/SettingWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BetterGFE.Core;
 using BetterGFE.Models;
+using Microsoft.Win32;
 
 namespace BetterGFE
 {
@@ -32,6 +33,40 @@ namespace BetterGFE
             irBlackList.ItemsSource = Config.Instance.AutoIrConfig.BlackList;
         }
 
+        public ICommand AddProgram
+        {
+            get => new DelegateCommand<List<ProcessInfo>>()
+            {
+                CanExecuteFunc = (list) => true,
+                ExecuteFunc = (list) =>
+                {
+                    if (list == null)
+                    {
+                        throw new ArgumentNullException(nameof(list));
+                    }
+                    else if (typeof(List<ProcessInfo>) != list.GetType())
+                    {
+                        throw new ArgumentException($"{nameof(list)} must be {typeof(List<ProcessInfo>)}");
+                    }
+
+                    var diag = new OpenFileDialog
+                    {
+                        Filter = "Executable File|*.exe",
+                    };
+                    if (diag.ShowDialog() != true)
+                    {
+                        return;
+                    }
+                    ((List<ProcessInfo>)list).Add(new ProcessInfo
+                    {
+                        FilePath = diag.FileName,
+                        ProcessName = diag.SafeFileName,
+                    });
+                    // TODO: リストを更新
+                }
+            };
+        }
+
         public ICommand SaveAndCloseCommand
         {
             get => new DelegateCommand()
@@ -40,7 +75,7 @@ namespace BetterGFE
                 ExecuteFunc = () =>
                 {
                     //Console.WriteLine("[BetterGFE] Push OK on config window");
-                    Config.Instance.GeneralConfig.RunOnStartup = runOnStartup.IsChecked??false;
+                    Config.Instance.GeneralConfig.RunOnStartup = runOnStartup.IsChecked ?? false;
 
                     Config.Instance.AutoIrConfig.WhiteList = (List<ProcessInfo>)irWhiteList.ItemsSource;
                     Config.Instance.AutoIrConfig.BlackList = (List<ProcessInfo>)irBlackList.ItemsSource;
